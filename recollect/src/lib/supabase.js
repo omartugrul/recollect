1dbb7ccbf22f149c84a7f03d78c12d5dc5e48ab7
Omar Anees <omarumbrage@Omars-MBP.attlocal.net>
2025-01-22 22:13:32 -0600

    finally fixed my .env issue

--- recollect/src/lib/supabase.js
+++ recollect/src/lib/supabase.js
@@ -1,19 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-//console.log("🔍 Supabase URL: test test test", import.meta.env.VITE_SUPABASE_URL);
-//console.log("🔍 Supabase Anon Key:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-
-console.log("🛠️ Checking Environment Variables:");
 console.log("VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
 console.log("VITE_SUPABASE_ANON_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 
 const supabase = createClient(
-//   import.meta.env.VITE_SUPABASE_URL,
-//   import.meta.env.VITE_SUPABASE_ANON_KEY
     import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY
-
 );
 
 export default supabase;
